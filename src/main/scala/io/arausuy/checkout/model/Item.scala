e42d64e4fc1ffac157ead1ae2c0e167f438b9136
Yusuf Faraji <yusuf.faraji@mail.com>
2017-12-04 21:34:06 +0000

    Add item models

---
+++ src/main/scala/io/arausuy/checkout/model/Item.scala
@@ -0,0 +1,26 @@
+package io.arausuy.checkout.model
+
+sealed trait Item
+
+case class A(price: BigDecimal) extends Item
+case class B(price: BigDecimal) extends Item
+case class C(price: BigDecimal) extends Item
+case class D(price: BigDecimal) extends Item
+
+
+
+object Item {
+
+  def apply(itemName: String, price: BigDecimal): Either[Throwable, Item] = {
+    itemName.toUpperCase match {
+      case "A" => Right(A(price))
+      case "B" => Right(B(price))
+      case "C" => Right(C(price))
+      case "D" => Right(D(price))
+      case _ => Left(UnknownItemException("Unable to create object"))
+    }
+  }
+
+  case class UnknownItemException(msg: String) extends Exception(msg)
+
+}
---
+++ src/test/scala/io/arausuy/checkout/model/ItemSpec.scala
@@ -0,0 +1,47 @@
+package io.arausuy.checkout.model
+
+import io.arausuy.checkout.model.Item.UnknownItemException
+import org.scalatest.{Matchers, WordSpecLike}
+
+
+class ItemSpec extends WordSpecLike with Matchers {
+
+  "Items" must {
+    "inform when an an attempt on creating a non-existing item" in {
+      val attempt = Item("G", BigDecimal(1.0))
+      attempt.isLeft shouldBe true
+      attempt.left.get shouldBe a[UnknownItemException]
+    }
+
+    "create an A" in {
+      val attempt = Item("A", BigDecimal(1.0))
+      val anA = A(BigDecimal(1.0))
+      attempt.isRight shouldBe true
+      attempt.right.get shouldBe anA
+    }
+    "create a B" in {
+      val attempt = Item("B", BigDecimal(1.0))
+      val b = B(BigDecimal(1.0))
+      attempt.isRight shouldBe true
+      attempt.right.get shouldBe b
+    }
+    "create a C" in {
+      val attempt = Item("C", BigDecimal(1.0))
+      val c = C(BigDecimal(1.0))
+      attempt.isRight shouldBe true
+      attempt.right.get shouldBe c
+    }
+    "create a D" in {
+      val attempt = Item("D", BigDecimal(1.0))
+      val d = D(BigDecimal(1.0))
+      attempt.isRight shouldBe true
+      attempt.right.get shouldBe d
+    }
+
+
+
+  }
+
+
+
+}
